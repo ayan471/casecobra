59d88293f46089d33b3efd7e01f2128151c11f3f
Ayan Pakhira <pakhiraayan@gmail.com>
2024-05-10 19:33:19 +0530

    built the app

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,5 +11,37 @@ export const formatPrice = (price: number) => {
     style: "currency",
     currency: "USD",
   });
+
   return formatter.format(price);
 };
+
+export function constructMetadata({
+  title = "CaseCobra - custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@AYANPAKHIRA1",
+    },
+    icons,
+    metadataBase: new URL("https://casecobra-xi.vercel.app/"),
+  };
+}
